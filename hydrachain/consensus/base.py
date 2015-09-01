5552cf1c27393faadf815a6476bee0c3bbdcaac7
Heiko Heiko <heiko@heiko.org>
2015-09-01 07:49:04 +0200

    consensus base classes

---
+++ hydrachain/consensus/base.py
@@ -0,0 +1,437 @@
+# Copyright (c) 2015 Heiko Hees
+try:
+    from ecdsa_recover import ecdsa_raw_sign, ecdsa_raw_recover
+except ImportError:
+    from bitcoin import ecdsa_raw_sign, ecdsa_raw_recover
+from collections import Counter
+from ethereum.blocks import Block
+from ethereum.utils import big_endian_to_int
+from bitcoin import encode_pubkey, N, P
+import rlp
+from rlp.sedes import big_endian_int, binary
+from rlp.sedes import CountableList
+from rlp.utils import encode_hex
+from ethereum.blocks import BlockHeader
+from ethereum.transactions import Transaction
+from hydrachain.utils import sha3, phx
+
+
+def ishash(h):
+    return isinstance(h, bytes) and len(h) == 32
+
+
+class InvalidSignature(Exception):
+    pass
+
+
+class RLPHashable(rlp.Serializable):
+
+    @property
+    def hash(self):
+        return sha3(rlp.encode(self))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.hash == other.hash
+
+    def __hash__(self):
+        return big_endian_to_int(self.hash)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return '<%s(%s)>' % (self.__class__.__name__, encode_hex(self.hash)[:4])
+
+
+class Signed(RLPHashable):
+
+    fields = [
+        ('v', big_endian_int),
+        ('r', big_endian_int),
+        ('s', big_endian_int),
+    ]
+
+    _sender = None
+
+    def __init__(self, *args, **kargs):
+        super(Signed, self).__init__(*args, **kargs)
+
+    def sign(self, privkey):
+        """Sign this with a private key.
+
+        A potentially already existing signature would be overridden.
+        """
+        if privkey in (0, '', '\x00' * 32):
+            raise InvalidSignature("Zero privkey cannot sign")
+        rawhash = sha3(rlp.encode(self, self.__class__.exclude(['v', 'r', 's'])))
+        self.v, self.r, self.s = ecdsa_raw_sign(rawhash, privkey)
+        self._sender = None
+        return self
+
+    @property
+    def sender(self):
+        if not self._sender:
+            # Determine sender
+            if self.v:
+                if self.r >= N or self.s >= P or self.v < 27 or self.v > 28 \
+                   or self.r == 0 or self.s == 0:
+                    raise InvalidSignature("Invalid signature values!")
+                rlpdata = rlp.encode(self, self.__class__.exclude(['v', 'r', 's']))
+                rawhash = sha3(rlpdata)
+                pub = ecdsa_raw_recover(rawhash, (self.v, self.r, self.s))
+                if pub is False:
+                    raise InvalidSignature("Invalid signature values (x^3+7 is non-residue)")
+                if pub == (0, 0):
+                    raise InvalidSignature("Invalid signature (zero privkey cannot sign)")
+                pub = encode_pubkey(pub, 'bin')
+                self._sender = sha3(pub[1:])[-20:]
+                assert self.sender == self._sender
+            else:
+                self._sender = None
+        return self._sender
+
+
+# Votes
+
+class Vote(Signed):
+
+    """A signed Vote"""
+
+    fields = [
+        ('height', big_endian_int),
+        ('round', big_endian_int),
+        ('blockhash', binary),
+    ] + Signed.fields
+
+    def __init__(self, height, round, blockhash='', v=0, r=0, s=0):
+        super(Vote, self).__init__(height, round, blockhash, v=v, r=r, s=s)
+
+        # restore class when deserialized
+        if blockhash:
+            assert ishash(blockhash)
+            self.blockhash = blockhash
+            self.__class__ = VoteBlock
+        else:
+            self.__class__ = VoteNil
+
+    @property
+    def hr(self):
+        return self.height, self.round
+
+
+class VoteBlock(Vote):
+    pass
+
+
+class VoteNil(Vote):
+    pass
+
+
+# LockSets
+
+class InvalidVote(Exception):
+    pass
+
+
+class DoubleVotingError(InvalidVote):
+    pass
+
+
+class LockSet(RLPHashable):  # careful, is mutable!
+
+    fields = [
+        ('num_eligible_votes', big_endian_int),
+        ('votes', CountableList(Vote))
+    ]
+
+    processed = False
+
+    def __init__(self, num_eligible_votes, votes=None):
+        self.num_eligible_votes = num_eligible_votes
+        self.votes = []
+        for v in votes or []:
+            self.add(v)
+
+    # @property
+    # def size(self):
+    #     return len(self.votes) * 67 + 5
+
+    def copy(self):
+        return LockSet(self.num_eligible_votes, self.votes)
+
+    @property
+    def state(self):
+        if not self.is_valid:
+            s = 'I'
+        elif self.has_quorum:
+            s = 'Q'
+        elif self.has_quorum_possible:
+            s = 'P'
+        elif self.has_noquorum:
+            s = 'N'
+        assert s
+        return '%s:%d' % (s, len(self))
+
+    def __repr__(self):
+        return '<LockSet(%s H:%d R:%d)>' % (self.state, self.height, self.round)
+
+    def add(self, vote, force_replace=False):
+        assert isinstance(vote, Vote)
+        if not vote.sender:
+            raise InvalidVote('no signature')
+        if vote not in self.votes:
+            if len(self) and self.hr != vote.hr:
+                raise InvalidVote('inconsistent height, round')
+            signee = self.signee
+            if vote.sender in signee:
+                if not force_replace:
+                    raise DoubleVotingError(vote.sender)  # different votes on the same H,R
+                self.votes.remove(self.votes[signee.index(vote.sender)])
+            self.votes.append(vote)
+            return True
+
+    def __len__(self):
+        return len(self.votes)
+
+    def __iter__(self):
+        return iter(self.votes)
+
+    @property
+    def signee(self):
+        return [v.sender for v in self.votes]
+
+    def blockhashes(self):
+        assert self.is_valid
+        c = Counter(v.blockhash for v in self.votes if isinstance(v, VoteBlock))
+        # deterministc sort necessary
+        return sorted(c.most_common(), cmp=lambda a, b: cmp((b[1], b[0]), (a[1], a[0])))
+
+    @property
+    def hr(self):
+        assert len(self), 'no votes, can not determin height'
+        h = set([(v.height, v.round) for v in self.votes])
+        assert len(h) == 1, len(h)
+        return h.pop()
+
+    height = property(lambda self: self.hr[0])
+    round = property(lambda self: self.hr[1])
+
+    @property
+    def is_valid(self):
+        return len(self) > 2 / 3. * self.num_eligible_votes and self.hr
+
+    @property
+    def has_quorum(self):
+        """
+        we've seen +2/3 of all eligible votes voting for one block.
+        there is a quorum.
+        """
+        assert self.is_valid
+        bhs = self.blockhashes()
+        print 'bhs', bhs, len(bhs)
+        if bhs and bhs[0][1] > 2 / 3. * self.num_eligible_votes:
+            assert self.has_quorum_possible
+            return bhs[0][0]
+        assert self.has_noquorum or self.has_quorum_possible
+
+    @property
+    def has_noquorum(self):
+        """
+        less than 1/3 of the known votes are on the same block
+        """
+        assert self.is_valid
+        bhs = self.blockhashes()
+        if not bhs or bhs[0][1] < 1 / 3. * self.num_eligible_votes:
+            assert not self.has_quorum_possible
+            return True
+
+    @property
+    def has_quorum_possible(self):
+        """
+        we've seen +1/3 of all eligible votes voting for one block.
+        at least one vote was from a honest node.
+        we can assume that this block is agreeable.
+        """
+        assert self.is_valid  # we could tell that earlier
+        bhs = self.blockhashes()
+        if bhs and bhs[0][1] > 1 / 3. * self.num_eligible_votes:
+            return bhs[0][0]
+
+
+############
+
+def genesis_signing_lockset(genesis, privkey):
+    """
+    in order to avoid a complicated bootstrapping, we define
+    the genesis_signing_lockset as a lockset with one vote by any validator.
+    """
+    v = VoteBlock(0, 0, genesis.hash)
+    v.sign(privkey)
+    ls = LockSet(num_eligible_votes=1)
+    ls.add(v)
+    assert ls.has_quorum
+    return ls
+
+
+class InvalidProposal(Exception):
+    pass
+
+
+class TransientBlock(rlp.Serializable):
+
+    """A partially decoded, unvalidated block."""
+
+    fields = [
+        ('header', BlockHeader),
+        ('transaction_list', rlp.sedes.CountableList(Transaction)),
+        ('uncles', rlp.sedes.CountableList(BlockHeader))
+    ]
+
+    def __init__(self, header, transaction_list, uncles):
+        self.header = header
+        self.transaction_list = transaction_list
+        self.uncles = uncles
+
+    def to_block(self, db, parent=None):
+        """Convert the transient block to a :class:`ethereum.blocks.Block`"""
+        return Block(self.header, self.transaction_list, self.uncles, db=db, parent=parent)
+
+
+class Proposal(Signed):
+    pass
+
+
+class BlockProposal(Proposal):
+
+    fields = [
+        ('height', big_endian_int),
+        ('round', big_endian_int),
+        ('block', TransientBlock),
+        ('signing_lockset', LockSet),
+        ('round_lockset', LockSet)
+    ] + Signed.fields
+
+    def __init__(self, height, round, block, signing_lockset,
+                 round_lockset=None, v=0, r=0, s=0):
+        """
+        if round == 0 the signing_lockset also proves,
+        that proposal is eligible and we need not round_lockset
+        """
+        assert isinstance(block, (Block, TransientBlock))
+        assert isinstance(signing_lockset, LockSet)
+        assert round_lockset is None or isinstance(round_lockset, LockSet)
+        assert round >= 0
+        assert height > 0
+        if round > 0 and not round_lockset:
+            raise InvalidProposal('R>0 needs a round lockset')
+        if round == 0 and round_lockset:
+            raise InvalidProposal('R0 must not have a round lockset')
+        self.height = height
+        self.round = round
+        self.block = block
+        self.signing_lockset = signing_lockset
+        self.round_lockset = round_lockset or LockSet(0)
+
+        super(BlockProposal, self).__init__(height, round, block, signing_lockset,
+                                            self.round_lockset, v, r, s)
+
+        if block.header.number != self.height:
+            raise InvalidProposal('lockset.height / block.height mismatch')
+        if self.round_lockset and height != self.round_lockset.height:
+            raise InvalidProposal('height mismatch')
+        if not (round > 0 or self.lockset.has_quorum):
+            raise InvalidProposal('R0 lockset == signing lockset needs quorum')
+        if not (round > 0 or self.lockset.height == block.number - 1):
+            raise InvalidProposal('R0 round lockset must be from previous height')
+        if not (round == 0 or round == self.lockset.round + 1):
+            raise InvalidProposal('Rn round lockset must be from previous round')
+        if not self.signing_lockset.has_quorum:
+            raise InvalidProposal('signing lockset needs quorum')
+        if not (self.signing_lockset.height == self.height - 1):
+            raise InvalidProposal('signing lockset height mismatch')
+        if self.round_lockset and not round_lockset.has_noquorum:
+            raise InvalidProposal('at R>0 can only propose if there is a NoQuorum for R-1')
+
+    @property
+    def lockset(self):
+        return self.round_lockset or self.signing_lockset
+
+    @property
+    def sender(self):
+        s = super(BlockProposal, self).sender
+        if not s:
+            raise InvalidProposal('signature missing')
+        if s != self.block.header.coinbase:
+            raise InvalidProposal('signature does not match coinbase')
+        return s
+
+    def validate_votes(self, validators_H, validators_prevH):
+        "set of validators may change between heights"
+        assert self.sender
+
+        def check(lockset, validators):
+            if not lockset.num_eligible_votes == len(validators):
+                raise InvalidProposal('lockset num_eligible_votes mismatch')
+            for v in lockset:
+                if v.sender not in validators:
+                    raise InvalidProposal('invalid signer')
+        if self.round_lockset:
+            check(self.round_lockset, validators_H)
+        check(self.signing_lockset, validators_prevH)
+
+        return True
+
+    def __repr__(self):
+        return "<%s %r B:%s>" % (self.__class__.__name__, self.sender, phx(self.blockhash))
+
+    @property
+    def blockhash(self):
+        return self.block.hash
+
+
+class VotingInstruction(Proposal):
+
+    fields = [
+        ('height', big_endian_int),
+        ('round', big_endian_int),
+        ('round_lockset', LockSet)
+    ] + Signed.fields
+
+    def __init__(self, height, round, round_lockset, v=0, r=0, s=0):
+        super(VotingInstruction, self).__init__(height, round, round_lockset, v, r, s)
+        if not round > 0:
+            raise InvalidProposal('VotingInstructions must have R>0')
+        if not self.lockset.has_quorum_possible:
+            raise InvalidProposal('VotingInstruction requires quorum possible')
+        if not (round == self.lockset.round + 1):
+            raise InvalidProposal('Rn round lockset must be from previous round')
+        if not (height == self.lockset.height):
+            raise InvalidProposal('height mismatch')
+        if not round > 0:
+            raise InvalidProposal('VotingInstructions must have R>0')
+        assert round == round_lockset.round + 1
+        assert height == round_lockset.height
+
+    @property
+    def blockhash(self):
+        return self.round_lockset.has_quorum_possible
+
+    @property
+    def lockset(self):
+        return self.round_lockset
+
+    def __repr__(self):
+        return "<%s %r B:%s>" % (self.__class__.__name__, self.sender, phx(self.blockhash))
+
+    def validate_votes(self, validators_H):
+        "set of validators may change between heights"
+        assert self.sender
+        if not self.round_lockset.num_eligible_votes == len(validators_H):
+            raise InvalidProposal('round_lockset num_eligible_votes mismatch')
+        for v in self.round_lockset:
+            if v.sender not in validators_H:
+                raise InvalidProposal('invalid signer')
+
+
+#########################
+# safeguard for forks: if there is a quorum on a block which has a wrong prevhash: panic!
---
+++ hydrachain/tests/test_base.py
@@ -0,0 +1,333 @@
+# from hydrachain import protocol
+from hydrachain.consensus.base import Vote, VoteBlock, VoteNil, LockSet, ishash
+from hydrachain.consensus.base import DoubleVotingError, InvalidVote
+from hydrachain.consensus.base import BlockProposal, genesis_signing_lockset, InvalidProposal
+from hydrachain.consensus.base import Proposal, VotingInstruction
+
+from ethereum import utils, tester
+import rlp
+import pytest
+
+
+def test_vote():
+    h, r = 2, 3
+    bh = '0' * 32
+    privkey = 'x' * 32
+    sender = utils.privtoaddr(privkey)
+
+    v = Vote(h, r)
+    v2 = Vote(h, r, blockhash=bh)
+
+    assert isinstance(v, Vote)
+    assert isinstance(v2, Vote)
+
+    assert isinstance(v, VoteNil)
+    assert isinstance(v, rlp.Serializable)
+
+    assert isinstance(v2, VoteBlock)
+
+    v.sign(privkey)
+    s = v.sender
+    assert s == sender
+
+    v2.sign(privkey)
+    assert v2.sender == sender
+
+    # encode
+    assert len(v.get_sedes()) == len(v.fields) == 6
+
+    vs = rlp.encode(v)
+    assert isinstance(vs, bytes)
+    print rlp.decode(vs)
+    vd = rlp.decode(vs, Vote)
+    assert isinstance(vd, VoteNil)
+    assert vd.blockhash == ''
+    assert vd == v
+
+    v2s = rlp.encode(v2)
+    v2d = rlp.decode(v2s, Vote)
+    assert isinstance(v2d, VoteBlock)
+    assert v2d.blockhash == bh
+    assert v2d == v2
+
+    assert v != v2
+    assert vd != v2d
+
+    assert len(set((v, vd))) == 1
+    assert len(set((v2, v2d))) == 1
+    assert len(set((v, vd, v2, v2d))) == 2
+
+
+privkeys = [chr(i) * 32 for i in range(1, 11)]
+validators = [utils.privtoaddr(p) for p in privkeys]
+
+
+def test_LockSet():
+    ls = LockSet(num_eligible_votes=len(privkeys))
+    assert not ls
+    assert len(ls) == 0
+
+    bh = '0' * 32
+    r, h = 2, 3
+    v1 = VoteBlock(h, r, bh)
+
+    # add not signed
+    with pytest.raises(InvalidVote):
+        ls.add(v1)
+    assert not ls
+    assert v1 not in ls
+
+    # add signed
+    v1.sign(privkeys[0])
+    ls.add(v1)
+
+    assert ls
+    assert len(ls) == 1
+    lsh = ls.hash
+    ls.add(v1)
+    assert lsh == ls.hash
+    assert len(ls) == 1
+
+    # second vote same sender
+    v2 = VoteBlock(h, r, bh)
+    v2.sign(privkeys[0])
+    ls.add(v1)
+    ls.add(v2)
+    assert lsh == ls.hash
+    assert len(ls) == 1
+
+    # third vote
+    v3 = VoteBlock(h, r, bh)
+    v3.sign(privkeys[1])
+    ls.add(v1)
+    ls.add(v3)
+    assert lsh != ls.hash
+    assert len(ls) == 2
+    assert v3 in ls
+
+    lsh = ls.hash
+
+    # vote wrong round
+    v4 = VoteBlock(h, r + 1, bh)
+    v4.sign(privkeys[2])
+    with pytest.raises(InvalidVote):
+        ls.add(v4)
+    assert lsh == ls.hash
+    assert len(ls) == 2
+    assert v4 not in ls
+
+    # vote twice
+    v3_2 = VoteBlock(h, r, blockhash='1' * 32)
+    v3_2.sign(privkeys[1])
+    with pytest.raises(DoubleVotingError):
+        ls.add(v3_2)
+    assert lsh == ls.hash
+    assert len(ls) == 2
+    assert v3_2 not in ls
+
+
+def test_one_vote_lockset():
+    ls = LockSet(num_eligible_votes=1)
+    bh = '0' * 32
+    r, h = 2, 3
+    v1 = VoteBlock(h, r, bh)
+    v1.sign(privkeys[0])
+    ls.add(v1)
+    assert ls.has_quorum
+
+
+def test_LockSet_isvalid():
+    ls = LockSet(num_eligible_votes=len(privkeys))
+    bh = '0' * 32
+    r, h = 2, 3
+
+    votes = [VoteBlock(h, r, bh) for i in range(len(privkeys))]
+    for i, v in enumerate(votes):
+        v.sign(privkeys[i])
+        ls.add(v)
+        assert len(ls) == i + 1
+        if len(ls) < ls.num_eligible_votes * 2 / 3.:
+            assert not ls.is_valid
+        else:
+            assert ls.is_valid
+            assert ls.has_quorum  # same blockhash
+
+
+def test_LockSet_quorums():
+    combinations = dict(has_quorum=[
+        [1] * 7,
+        [1] * 7 + [2] * 3,
+        [1] * 7 + [None] * 3,
+    ],
+        has_noquorum=[
+        [1] * 3 + [2] * 3 + [None],
+        [None] * 7,
+        [None] * 10,
+        range(10),
+        range(7)
+    ],
+        has_quorum_possible=[
+        [1] * 4 + [None] * 3,
+        [1] * 4 + [2] * 4,
+        [1] * 4 + [2] * 3 + [3] * 3,
+        [1] * 6 + [2]
+    ])
+
+    r, h = 1, 2
+    for method, permutations in combinations.items():
+        for set_ in permutations:
+            assert len(set_) >= 7
+            ls = LockSet(len(privkeys))
+            for i, p in enumerate(set_):
+                if p is not None:
+                    bh = chr(p) * 32
+                    v = VoteBlock(h, r, bh)
+                else:
+                    v = VoteNil(h, r)
+                v.sign(privkeys[i])
+                ls.add(v)
+            assert len(ls) >= 7
+            assert getattr(ls, method)
+
+            # check stable sort
+            bhs = ls.blockhashes()
+            if len(bhs) > 1:
+                assert ishash(bhs[0][0])
+                assert isinstance(bhs[0][1], int)
+                if bhs[0][1] == bhs[1][1]:
+                    assert bhs[0][0] > bhs[1][0]
+                else:
+                    assert bhs[0][1] > bhs[1][1]
+
+            # test serialization
+
+            s = rlp.encode(ls)
+            d = rlp.decode(s, LockSet)
+
+            assert ls == d
+            assert id(ls) != id(d)
+            assert getattr(ls, method) == getattr(d, method)
+
+
+def test_blockproposal():
+    s = tester.state()
+
+    # block 1
+    s.mine(n=1)
+    genesis = s.blocks[0]
+    assert genesis.header.number == 0
+    blk1 = s.blocks[1]
+    assert blk1.header.number == 1
+    gls = genesis_signing_lockset(genesis, privkeys[0])
+    bp = BlockProposal(height=1, round=0, block=blk1, signing_lockset=gls, round_lockset=None)
+    assert bp.lockset == gls
+    assert isinstance(bp, Proposal)
+    bp.sign(tester.k0)
+
+    with pytest.raises(InvalidProposal):  # round >0 needs round_lockset
+        bp = BlockProposal(height=1, round=1, block=blk1, signing_lockset=gls, round_lockset=None)
+    bp.validate_votes(validators, validators[:1])
+
+    # block 2
+    s.mine(n=1)
+    blk2 = s.blocks[2]
+    assert blk2.header.number == 2
+
+    ls = LockSet(len(validators))
+    for privkey in privkeys:
+        v = VoteBlock(height=1, round=0, blockhash=blk1.hash)
+        v.sign(privkey)
+        ls.add(v)
+
+    bp = BlockProposal(height=2, round=0, block=blk2, signing_lockset=ls, round_lockset=None)
+    assert bp.lockset == ls
+    with pytest.raises(InvalidProposal):  # signature missing
+        bp.validate_votes(validators, validators)
+
+    bp.sign(privkeys[0])
+    with pytest.raises(InvalidProposal):  # round >0 needs round_lockset
+        bp.validate_votes(validators, validators)
+
+    bp.sign(tester.k0)
+    bp.validate_votes(validators, validators)
+
+    with pytest.raises(InvalidProposal):  # round >0 needs round_lockset
+        bp = BlockProposal(height=2, round=1, block=blk2, signing_lockset=gls, round_lockset=None)
+
+    # block 2 round 1, timeout in round=0
+    rls = LockSet(len(validators))
+    for privkey in privkeys:
+        v = VoteNil(height=2, round=0)
+        v.sign(privkey)
+        rls.add(v)
+    bp = BlockProposal(height=2, round=1, block=blk2, signing_lockset=ls, round_lockset=rls)
+    assert bp.lockset == rls
+    bp.sign(tester.k0)
+    bp.validate_votes(validators, validators)
+
+    # serialize
+    s = rlp.encode(bp)
+    dbp = rlp.decode(s, BlockProposal)
+    assert dbp.block == blk2
+
+    dbp.validate_votes(validators, validators)
+
+    # check quorumpossible lockset failure
+    rls = LockSet(len(validators))
+    for i, privkey in enumerate(privkeys):
+        if i < 4:
+            v = VoteBlock(height=2, round=0, blockhash='0' * 32)
+        else:
+            v = VoteNil(height=2, round=0)
+        v.sign(privkey)
+        rls.add(v)
+    assert not rls.has_noquorum
+    assert rls.has_quorum_possible
+    with pytest.raises(InvalidProposal):  # NoQuorum necessary R0
+        bp = BlockProposal(height=2, round=1, block=blk2, signing_lockset=ls, round_lockset=rls)
+
+
+def test_VotingInstruction():
+    rls = LockSet(len(validators))
+    bh = '1' * 32
+    for i, privkey in enumerate(privkeys):
+        if i < 4:  # quorum possible
+            v = VoteBlock(height=2, round=0, blockhash=bh)
+
+        else:
+            v = VoteNil(height=2, round=0)
+        v.sign(privkey)
+        rls.add(v)
+    assert rls.has_quorum_possible
+    bp = VotingInstruction(height=2, round=1, round_lockset=rls)
+    bp.sign(privkeys[0])
+    assert bh == bp.blockhash
+
+    # noquorum
+    rls = LockSet(len(validators))
+    for i, privkey in enumerate(privkeys):
+        if i < 3:  # noquorum possible
+            v = VoteBlock(height=2, round=0, blockhash=bh)
+        else:
+            v = VoteNil(height=2, round=0)
+        v.sign(privkey)
+        rls.add(v)
+    assert not rls.has_quorum_possible
+    assert rls.has_noquorum
+    with pytest.raises(InvalidProposal):  # QuorumPossiblle necessary R0
+        bp = VotingInstruction(height=2, round=1, round_lockset=rls)
+
+    # noquorum
+    rls = LockSet(len(validators))
+    for i, privkey in enumerate(privkeys):
+        if i < 3:  # noquorum possible
+            v = VoteBlock(height=2, round=0, blockhash=bh)
+        else:
+            v = VoteNil(height=2, round=0)
+
+        v.sign(privkey)
+        rls.add(v)
+    assert not rls.has_quorum_possible
+    assert rls.has_noquorum
+    with pytest.raises(InvalidProposal):  # QuorumPossiblle necessary R0
+        bp = VotingInstruction(height=2, round=1, round_lockset=rls)
