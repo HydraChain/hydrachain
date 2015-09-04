dff7cab29ff616368a563635cfff00cffaa15ba0
Heiko Heiko <heiko@heiko.org>
2015-09-04 15:22:12 +0200

    hdc protocol

---
+++ hydrachain/consensus/protocol.py
@@ -0,0 +1,126 @@
+import rlp
+import gevent
+import time
+from devp2p.protocol import BaseProtocol, SubProtocolError
+from ethereum.transactions import Transaction
+from hydrachain.consensus.base import BlockProposal, VotingInstruction, Vote
+from ethereum import slogging
+log = slogging.get_logger('protocol.hdc')
+
+
+class HDCProtocolError(SubProtocolError):
+    pass
+
+
+class HDCProtocol(BaseProtocol):
+
+    """
+    HydraChain Wire Protocol
+    """
+    protocol_id = 1
+    network_id = 0
+    max_cmd_id = 15  # FIXME
+    name = 'hdc'
+    version = 1
+
+    def __init__(self, peer, service):
+        # required by P2PProtocol
+        self.config = peer.config
+        BaseProtocol.__init__(self, peer, service)
+
+    class status(BaseProtocol.command):
+
+        """
+        protocolVersion: The version of the Ethereum protocol this peer implements. 30 at present.
+        networkID: The network version of Ethereum for this peer. 0 for the official testnet.
+        totalDifficulty: Total Difficulty of the best chain. Integer, as found in block header.
+        latestHash: The hash of the block with the highest validated total difficulty.
+        GenesisHash: The hash of the Genesis block.
+        """
+        cmd_id = 0
+        sent = False
+
+        structure = [
+            ('eth_version', rlp.sedes.big_endian_int),
+            ('network_id', rlp.sedes.big_endian_int),
+            ('chain_difficulty', rlp.sedes.big_endian_int),
+            ('chain_head_hash', rlp.sedes.binary),
+            ('genesis_hash', rlp.sedes.binary)]
+
+        def create(self, proto, chain_difficulty, chain_head_hash, genesis_hash):
+            self.sent = True
+            network_id = proto.service.app.config['hdc'].get('network_id', proto.network_id)
+            return [proto.version, network_id, chain_difficulty, chain_head_hash, genesis_hash]
+
+    class transactions(BaseProtocol.command):
+
+        """
+        Specify (a) transaction(s) that the peer should make sure is included on its transaction
+        queue. The items in the list (following the first item 0x12) are transactions in the
+        format described in the main Ethereum specification. Nodes must not resend the same
+        transaction to a peer in the same session. This packet must contain at least one (new)
+        transaction.
+        """
+        cmd_id = 1
+        structure = rlp.sedes.CountableList(Transaction)
+
+        # todo: bloomfilter: so we don't send tx to the originating peer
+
+        @classmethod
+        def decode_payload(cls, rlp_data):
+            # convert to dict
+            txs = []
+            for i, tx in enumerate(rlp.decode_lazy(rlp_data)):
+                txs.append(Transaction.deserialize(tx))
+                if not i % 10:
+                    gevent.sleep(0.0001)
+            return txs
+
+    class getblocks(BaseProtocol.command):
+
+        """
+        Requests a Blocks message detailing a number of blocks to be sent, each referred to
+        by block number. Note: Don't expect that the peer necessarily give you all these blocks
+        in a single message - you might have to re-request them.
+        """
+        cmd_id = 2
+        structure = rlp.sedes.CountableList(rlp.sedes.big_endian_int)
+
+    class blocks(BaseProtocol.command):
+
+        """
+        BlockProposals sent in response to a getblocks request
+        """
+        cmd_id = 3
+        structure = rlp.sedes.CountableList(BlockProposal)
+
+        @classmethod
+        def encode_payload(cls, list_of_rlp):
+            """
+            rlp data directly from the database
+            """
+            return rlp.encode([rlp.codec.RLPData(x) for x in list_of_rlp], infer_serializer=False)
+
+    class blockproposal(BaseProtocol.command):
+
+        """
+        Specify a single BlockProposal that the peer should know about.
+        """
+        cmd_id = 4
+        structure = [('block', BlockProposal)]
+
+    class votinginstruction(BaseProtocol.command):
+
+        """
+        Specify a single VotingInstruction that the peer should know about.
+        """
+        cmd_id = 5
+        structure = [('votinginstruction', VotingInstruction)]
+
+    class vote(BaseProtocol.command):
+
+        """
+        Specify a single Vote that the peer should know about.
+        """
+        cmd_id = 6
+        structure = [('vote', Vote)]
---
+++ hydrachain/tests/test_hdc_protocol.py
@@ -0,0 +1,215 @@
+import rlp
+from ethereum import tester
+from ethereum import utils
+from devp2p.service import WiredService
+from devp2p.protocol import BaseProtocol
+from devp2p.app import BaseApp
+from hydrachain.consensus.protocol import HDCProtocol
+from hydrachain.consensus.base import genesis_signing_lockset, VoteNil, VoteBlock, LockSet
+from hydrachain.consensus.base import VotingInstruction, BlockProposal, TransientBlock
+
+
+tester.disable_logging()
+
+
+class PeerMock(object):
+    packets = []
+    config = dict()
+
+    def send_packet(self, packet):
+        self.packets.append(packet)
+
+
+def setup():
+    peer = PeerMock()
+    proto = HDCProtocol(peer, WiredService(BaseApp()))
+    proto.service.app.config['hdc'] = dict(network_id=1337)
+    chain = tester.state()
+    cb_data = []
+
+    def cb(proto, **data):
+        cb_data.append((proto, data))
+    return peer, proto, chain, cb_data, cb
+
+
+def test_basics():
+    peer, proto, chain, cb_data, cb = setup()
+
+    assert isinstance(proto, BaseProtocol)
+
+    d = dict()
+    d[proto] = 1
+    assert proto in d
+    assert d[proto] == 1
+    assert not proto
+    proto.start()
+    assert proto
+
+
+def test_status():
+    peer, proto, chain, cb_data, cb = setup()
+    genesis = head = chain.blocks[-1]
+
+    # test status
+    proto.send_status(
+        chain_difficulty=head.chain_difficulty(),
+        chain_head_hash=head.hash,
+        genesis_hash=genesis.hash
+    )
+    packet = peer.packets.pop()
+    proto.receive_status_callbacks.append(cb)
+    proto._receive_status(packet)
+
+    _p, _d = cb_data.pop()
+    assert _p == proto
+    assert isinstance(_d, dict)
+    assert _d['chain_difficulty'] == head.chain_difficulty()
+    print _d
+    assert _d['chain_head_hash'] == head.hash
+    assert _d['genesis_hash'] == genesis.hash
+    assert 'eth_version' in _d
+    assert 'network_id' in _d
+
+
+privkeys = [chr(i) * 32 for i in range(1, 11)]
+validators = [utils.privtoaddr(p) for p in privkeys]
+
+
+def create_proposal(blk):
+    signing_lockset = LockSet(len(validators))
+    for privkey in privkeys:
+        v = VoteBlock(blk.number - 1, 0, blk.hash)
+        v.sign(privkey)
+        signing_lockset.add(v)
+    bp = BlockProposal(height=blk.number, round=0, block=blk,
+                       signing_lockset=signing_lockset, round_lockset=None)
+    bp.sign(tester.k0)
+    return bp
+
+
+def test_blocks():
+    peer, proto, chain, cb_data, cb = setup()
+    gls = genesis_signing_lockset(chain.blocks[0], privkeys[0])
+
+    # test blocks
+    chain.mine(n=2)
+    assert len(chain.blocks) == 3
+    proposals = [create_proposal(b) for b in chain.blocks[1:]]
+    payload = [rlp.encode(p) for p in proposals]
+    proto.send_blocks(*payload)
+    packet = peer.packets.pop()
+    assert len(rlp.decode(packet.payload)) == 2
+
+    def list_cb(proto, blocks):
+        cb_data.append((proto, blocks))
+
+    proto.receive_blocks_callbacks.append(list_cb)
+    proto._receive_blocks(packet)
+
+    _p, proposals = cb_data.pop()
+    assert isinstance(proposals, list)
+    for proposal in proposals:
+        assert isinstance(proposal, BlockProposal)
+        assert proposal.height == proposal.block.header.number
+        assert isinstance(proposal.block, TransientBlock)
+        assert isinstance(proposal.block.transaction_list, list)
+        assert isinstance(proposal.block.uncles, list)
+        # assert that transactions and uncles have not been decoded
+        assert len(proposal.block.transaction_list) == 0
+        assert len(proposal.block.uncles) == 0
+
+
+def test_blockproposal():
+    pass
+
+
+def test_votinginstruction():
+    peer, proto, chain, cb_data, cb = setup()
+    height = 1
+    round = 0
+    bh = '1' * 32
+    round_lockset = LockSet(len(validators))
+    for privkey in privkeys:
+        v = VoteBlock(height, 0, bh)
+        v.sign(privkey)
+        round_lockset.add(v)
+    bp = VotingInstruction(height=height, round=1, round_lockset=round_lockset)
+    bp.sign(tester.k0)
+
+    payload = bp
+
+    proto.send_votinginstruction(payload)
+    packet = peer.packets.pop()
+    assert len(rlp.decode(packet.payload)) == 1
+
+    def list_cb(proto, votinginstruction):
+        cb_data.append((proto, votinginstruction))
+
+    proto.receive_votinginstruction_callbacks.append(list_cb)
+    proto._receive_votinginstruction(packet)
+
+    _p, vi = cb_data.pop()
+    assert vi == bp
+
+
+def test_getblocks():
+    peer, proto, chain, cb_data, cb = setup()
+    payload = range(10)
+    proto.send_getblocks(*payload)
+    packet = peer.packets.pop()
+    assert len(rlp.decode(packet.payload)) == len(payload)
+
+    def list_cb(proto, blocks):
+        cb_data.append((proto, blocks))
+
+    proto.receive_getblocks_callbacks.append(list_cb)
+    proto._receive_getblocks(packet)
+    _p, data = cb_data.pop()
+    assert data == payload
+
+
+def test_vote():
+    peer, proto, chain, cb_data, cb = setup()
+
+    def list_cb(proto, vote):
+        cb_data.append((proto, vote))
+    proto.receive_vote_callbacks.append(list_cb)
+
+    # VoteBlock
+    payload = v = VoteBlock(1, 0, '0' * 32)
+    v.sign(privkeys[0])
+    proto.send_vote(payload)
+    packet = peer.packets.pop()
+    proto._receive_vote(packet)
+    _p, data = cb_data.pop()
+    assert data == payload
+    assert isinstance(data, VoteBlock)
+
+    payload = v = VoteNil(1, 0)
+    v.sign(privkeys[0])
+    proto.send_vote(payload)
+    packet = peer.packets.pop()
+    proto._receive_vote(packet)
+    _p, data = cb_data.pop()
+    assert data == payload
+    assert isinstance(data, VoteNil)
+
+
+def _nono():
+    # newblock
+    approximate_difficulty = chain.blocks[-1].difficulty * 3
+    proto.send_newblock(block=chain.blocks[-1], chain_difficulty=approximate_difficulty)
+    packet = peer.packets.pop()
+    proto.receive_newblock_callbacks.append(cb)
+    proto._receive_newblock(packet)
+
+    _p, _d = cb_data.pop()
+    assert 'block' in _d
+    assert 'chain_difficulty' in _d
+    assert _d['chain_difficulty'] == approximate_difficulty
+    assert _d['block'].header == chain.blocks[-1].header
+    assert isinstance(_d['block'].transaction_list, list)
+    assert isinstance(_d['block'].uncles, list)
+    # assert that transactions and uncles have not been decoded
+    assert len(_d['block'].transaction_list) == 0
+    assert len(_d['block'].uncles) == 0
