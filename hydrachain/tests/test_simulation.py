f614fb9c5f1899fb2b449367735fe5a02284b482
Heiko Heiko <heiko@heiko.org>
2015-09-07 00:04:23 +0200

    tests

--- hydrachain/tests/test_simulation.py
+++ hydrachain/tests/test_simulation.py
@@ -1,14 +1,6 @@
 import pytest
-import rlp
-from ethereum import tester
-from ethereum import utils
-from devp2p.service import WiredService
-from devp2p.protocol import BaseProtocol
-from devp2p.app import BaseApp
-from hydrachain.consensus.protocol import HDCProtocol
-from hydrachain.consensus.base import genesis_signing_lockset, VoteNil, VoteBlock, LockSet
-from hydrachain.consensus.base import VotingInstruction, BlockProposal, TransientBlock
-from hydrachain.consensus.simulation import PeerMock, AppMock, Network
+from hydrachain.consensus.manager import RoundManager
+from hydrachain.consensus.simulation import Network
 import gevent
 
 gevent.get_hub().SYSTEM_ERROR = BaseException
@@ -33,20 +25,42 @@ def test_basic_simenv():
 
 
 def test_failing_validators():
-    network = Network(num_nodes=10)
+    network = Network(num_nodes=10, simenv=True)
     network.connect_nodes()
     network.disable_validators(num=3)
     network.start()
-    network.run(30)
+    network.run(10)
     network.check_consistency()
 
 
 def test_slow_validators():
-    pass
+    network = Network(num_nodes=10, simenv=True)
+    network.connect_nodes()
+    network.throttle_validators(num=3)
+    network.start()
+    network.run(10)
+    network.check_consistency()
 
 
 def test_slow_and_failing_validators():
-    pass
+    network = Network(num_nodes=10, simenv=True)
+    network.connect_nodes()
+    network.disable_validators(num=3)
+    network.throttle_validators(num=6)
+    network.start()
+    network.run(10)
+    network.check_consistency()
+
+
+def test_low_timeout():
+    orig_timeout = RoundManager.timeout
+    RoundManager.timeout = 0.1
+    network = Network(num_nodes=10, simenv=True)
+    network.connect_nodes()
+    network.start()
+    network.run(10)
+    network.check_consistency()
+    RoundManager.timeout = orig_timeout
 
 
 def test_resyncing_of_peers():
@@ -55,6 +69,37 @@ def test_resyncing_of_peers():
 
 def test_successive_joining():
     # bootstrap scenario
-    # manually connect peers
-    # nodes need to repeat their votes
-    pass
+
+    # this works without repeated VoteNil sending, as the first node will
+    # eventually collect a valid Lockset.
+    # if:
+    # nodes can request proposals, they missed
+    # the network is not disjoint at the beginning
+
+    # solution:
+    #   send current and last valid lockset and proposal with status
+
+    network = Network(num_nodes=3)
+    RoundManager.timeout = 1
+
+    # disable nodes, i.e. they won't connect yet
+    for n in network.nodes:
+        n.isactive = False
+
+    for n in network.nodes:
+        n.isactive = True
+        network.connect_nodes()
+        network.start()
+        network.run(2)
+
+    network.check_consistency()
+
+
+"""
+ToDo:
+    broadcasts
+    test with only few peers
+    broadcast filters
+    syncing
+
+"""
