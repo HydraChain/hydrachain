92ca00dd7f628d01cea3e03b191ec667289e0c6f
Ulrich Petri <ulo@ulo.pe>
2015-09-24 19:32:19 +0200

    [docker] Add "bootstrap" node container

--- docker/dev/hydrachain/node/start.sh
+++ docker/dev/hydrachain/node/start.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 set -e
 
-SEED=23
 # Wait for all nodes to come up
 /root/settle_file.py /etc/hosts
+
+cat /etc/hosts
+
+SEED=${SEED:-23}
 HOST_BASE_NAME=hydrachain_node_
+BOOTSTRAP_NODE_NAME=${HOST_BASE_NAME}bootstrap
+
+if ! grep -q $BOOTSTRAP_NODE_NAME /etc/hosts ; then
+    echo "No bootstrap node found. Aborting."
+    exit 1
+fi
 
 OWN_IP=$(ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}')
 OWN_INDEX=$(egrep "$OWN_IP\s+${HOST_BASE_NAME}" /etc/hosts | grep -v bridge | sed -r "s/^.*${HOST_BASE_NAME}//")
 OWN_NAME=${HOST_BASE_NAME}${OWN_INDEX}
-OWN_INDEX=$(($OWN_INDEX-1))
 NODE_COUNT=$(grep ${HOST_BASE_NAME} /etc/hosts | grep -v bridge | sed -r "s/^.*${HOST_BASE_NAME}//" | sort -n | tail -n1)
+# Increment node count to account for bootstrap node
+NODE_COUNT=$((NODE_COUNT+1))
+BOOTSTRAP_NODE=$(/root/mk_enode.py --host ${BOOTSTRAP_NODE_NAME} ${SEED} 0)
 
-BOOTSTRAP_NODE=$(/root/mk_enode.py --host ${HOST_BASE_NAME}${NODE_COUNT} ${SEED} ${NODE_COUNT})
+if [ ${OWN_NAME} == ${BOOTSTRAP_NODE_NAME} ]; then
+    OWN_INDEX=0
+fi
 
-cat /etc/hosts
+(set -o posix; set)
 
 cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/${OWN_NAME}/g" app.json
 /usr/bin/pm2 start ./app.json
 
+pip uninstall -y pyethapp
+rm -rf /pyethapp
+cp -a /pyethapp.src /pyethapp
+cd /pyethapp
+pip install -e .
+
 cp -a /hydrachain.src /hydrachain
 cd /hydrachain
 pip install -e .
 
-echo /usr/local/bin/hydrachain --bootstrap_node "$BOOTSTRAP_NODE" runlocal --num_validators ${NODE_COUNT} --node_num ${OWN_INDEX} --seed ${SEED}
-/usr/local/bin/hydrachain --bootstrap_node "$BOOTSTRAP_NODE" runlocal --num_validators ${NODE_COUNT} --node_num ${OWN_INDEX} --seed ${SEED}
+echo /usr/local/bin/hydrachain --bootstrap_node "$BOOTSTRAP_NODE" -l:debug,jsonrpc:info "$@" runlocal --num_validators ${NODE_COUNT} --node_num ${OWN_INDEX} --seed ${SEED}
+/usr/local/bin/hydrachain --bootstrap_node "$BOOTSTRAP_NODE" -l:debug,jsonrpc:info "$@" runlocal --num_validators ${NODE_COUNT} --node_num ${OWN_INDEX} --seed ${SEED}
