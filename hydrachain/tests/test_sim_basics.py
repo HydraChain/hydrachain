4a33ddee491ae21423f8ab10daaa99d0b906bce3
Heiko Heiko <heiko@heiko.org>
2015-10-11 11:46:25 +0200

    test for single node

--- hydrachain/tests/test_sim_basics.py
+++ hydrachain/tests/test_sim_basics.py
@@ -21,5 +21,17 @@ def test_basic_simenv():
     network.run(5)
     r = network.check_consistency()
     assert_maxrounds(r)
-    assert_heightdistance(r)
+    assert_heightdistance(r, max_distance=1)
     assert_blocktime(r, 0.5)
+
+
+def test_basic_singlenode():
+    network = Network(num_nodes=1, simenv=True)
+    network.connect_nodes()
+    network.normvariate_base_latencies()
+    network.start()
+    network.run(5)
+    r = network.check_consistency()
+    assert_maxrounds(r)
+    assert_heightdistance(r)
+    assert_blocktime(r, 1.5)
