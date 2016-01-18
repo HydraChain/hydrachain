d2e4e7842e679fa2440a28a1cf1a3d90aecb3d69
Heiko Heiko <heiko@heiko.org>
2016-01-18 13:28:28 +0000

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requirements = [
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '0.1.11'
+version = '0.1.12'
 
 
 setup(
