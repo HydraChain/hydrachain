9a4542448bf702189ce87fd493db612e69dbd8b7
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2016-01-20 21:40:34 +0100

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requirements = [
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '0.1.14'
+version = '0.1.15'
 
 
 setup(
