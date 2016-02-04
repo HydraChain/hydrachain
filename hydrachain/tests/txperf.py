9c95ab731154283a95ac12c3316edec5136b0aa9
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2016-02-04 18:46:30 +0100

    Add simple stats-reporting to txperf

--- hydrachain/tests/txperf.py
+++ hydrachain/tests/txperf.py
@@ -1,7 +1,8 @@
 """Example call:
 
-    >>> rm -rf /tmp/txperf && hydrachain -d /tmp/txperf runmultiple -v 3 & \
-            sleep 15 && time python hydrachain/tests/txperf.py && kill -9 %1
+    >>> NODES=3; rm -rf /tmp/txperf && hydrachain -d /tmp/txperf runmultiple \
+            -v $NODES > /dev/null 2>&1 & sleep 15 && time python \
+            hydrachain/tests/txperf.py && kill -9 %1
 """
 from pyethapp.rpc_client import JSONRPCClient
 
@@ -21,6 +22,7 @@ def do_tx(client, nonce, coinbase):
 
 def main(num_clients, num_txs=1):
     import time
+    start = time.time()
     txs = set()
     clients = [JSONRPCClient(4000 + i) for i in range(num_clients)]
     coinbase = clients[0].coinbase
@@ -29,6 +31,7 @@ def main(num_clients, num_txs=1):
         txh = do_tx(clients[0], nonce=nonce + i, coinbase=coinbase)
         print 'tx', i, txh
         txs.add(txh)
+    took = time.time() - start
     assert len(txs) == num_txs
 
     print 'checking if all %d txs are included' % num_txs
@@ -45,6 +48,7 @@ def main(num_clients, num_txs=1):
     assert len(tx_blocks) == num_txs
     print
     print 'Success: %d txs in %d blocks in all %d clients' % (num_txs, len(blocks), num_clients)
+    print "took %.2f s for %s tx ==> %.2f tx/s" % (took, num_txs, num_txs / took)
 
 
 if __name__ == '__main__':
